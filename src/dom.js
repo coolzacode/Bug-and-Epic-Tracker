87d45bc8fd361015610725b77de4ed0c13fc85a3
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-06-10 19:49:53 -0500

    feat: display Tickets

--- src/dom.js
+++ src/dom.js
@@ -10,7 +10,7 @@ export function displayEpics(epics) {
 
         epicButton.type = 'button';
         epicButton.className = 'epic-select-btn';
-        epicButton.dataset.epicID = `${epic.id}`;
+        epicButton.dataset.epicId = `${epic.id}`;
         epicButton.textContent = `Epic ${index}: ${epic.name}`;
 
         epicList.appendChild(epicButton);
@@ -19,3 +19,44 @@ export function displayEpics(epics) {
     epicContainer.appendChild(fragment);
 }
 
+export function displayTickets(epics, id) {
+    const ticketContainer = document.getElementById('ticket-grid-container');
+    ticketContainer.replaceChildren();
+    const fragment = document.createDocumentFragment();
+
+    const activeEpic = epics.find(epic => epic.id === id);
+
+    if (activeEpic === undefined) return;
+
+    activeEpic.tickets.forEach(ticket => {
+        const ticketCard = document.createElement('li');
+        const ticketTitle = document.createElement('h3');
+        const ticketPriorityBadge = document.createElement('span');
+        const ticketDue = document.createElement('p');
+
+        const ticketActions = document.createElement('div');
+        const viewButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+
+        ticketCard.className = `ticket-card ${ticket.priority}`;
+        ticketTitle.className = 'ticket-title';
+        ticketPriorityBadge.className = `ticket-priority-badge ${ticket.priority}`;
+        ticketDue.className = 'ticket-due';
+        ticketActions.type = 'button';
+        ticketActions.className = 'ticket-actions';
+        viewButton.className = 'btn secondary btn-view-ticket';
+        deleteButton.className = 'btn danger btn-delete-ticket';
+
+        ticketTitle.textContent = `${ticket.title}`;
+        ticketPriorityBadge.textContent = `${ticket.priority.charAt(0).toUpperCase() + ticket.priority.slice(1)}`;
+        ticketDue.textContent = `${ticket.dueDate}`;
+        viewButton.textContent = 'View/Edit';
+        deleteButton.textContent = 'Delete';
+
+        ticketActions.append(viewButton, deleteButton);
+        ticketCard.append(ticketTitle, ticketPriorityBadge, ticketDue, ticketActions);
+        fragment.appendChild(ticketCard);
+    });
+    ticketContainer.appendChild(fragment);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './styles.css';
 import { Epic, Ticket } from './models.js';
 import { epicObjects, addEpic, addTicketToEpic, deleteTicket } from './manager.js'
-import { epicContainer, displayEpics } from './dom.js';
+import { epicContainer, displayEpics, displayTickets } from './dom.js';
 
+const authEpic = addEpic("auth System");
+
+
+const bug1 = new Ticket('Fix login crash', '06-08-2026', 'critical');
+const bug2 = new Ticket('Update border colors', '06-10-2026', 'low');
+
+authEpic.tickets.push(bug1);
+authEpic.tickets.push(bug2);
+
+displayEpics(epicObjects);
+
+
+epicContainer.addEventListener('click', (e) => {
+    const target = e.target;
+    const targetId = target.dataset.epicId;
+
+    console.log(target);
+    console.log(targetId);
+
+    displayTickets(epicObjects, targetId);
+});
