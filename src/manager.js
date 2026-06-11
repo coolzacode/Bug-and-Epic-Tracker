f7a3d91d40145649aa91f21fcbb46aa55a874579
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-06-11 16:17:37 -0500

    refactor: add new logic and improve manager.js

--- src/manager.js
+++ src/manager.js
@@ -4,22 +4,39 @@ export let epicObjects = [
     new Epic('Uncategorized Issues')
 ];
 
+export let activeEpicId = epicObjects[0].id;
+
+export function setActiveEpicId(id) {
+    activeEpicId = id;
+}
+
+export function getActiveEpic() {
+    return epicObjects.find(epic => epic.id === activeEpicId);
+}
+
 export function addEpic(name) {
     const newEpic = new Epic(name.toString());
     epicObjects.push(newEpic);
-
     return newEpic;
 }
 
-export function addTicketToEpic(epic, ticket) {
-    epic.tickets.push(ticket);
-    return ticket;
+export function addTicketToEpic(epicId, ticketData) {
+    const epic = epicObjects.find(epic => epic.id === epicId);
+    if (!epic) return;
+
+    const newTicket = new Ticket(
+        ticketData.title,
+        ticketData.dueDate,
+        ticketData.priority,
+        ticketData.description
+    );
+
+    epic.tickets.push(newTicket);
+    return newTicket;
 }
 
 export function deleteTicket(epicId, ticketId) {
     const epic = epicObjects.find(epic => epic.id === epicId);
-
     if (!epic) return;
-
     epic.tickets = epic.tickets.filter(ticket => ticket.id !== ticketId);
 }
