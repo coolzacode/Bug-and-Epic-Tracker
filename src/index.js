a216e7aa12bc73176263a77f0329b545cb937a36
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-06-10 20:47:41 -0500

    feat: create and display new Epics

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import { Epic, Ticket } from './models.js';
 import { epicObjects, addEpic, addTicketToEpic, deleteTicket } from './manager.js'
 import { epicContainer, displayEpics, displayTickets } from './dom.js';
 
-const authEpic = addEpic("auth System");
+const createEpic = document.getElementById('btn-new-epic');
+const epicModal = document.getElementById('epic-modal');
+const ticketModal = document.getElementById('ticket-modal');
+const epicForm = document.getElementById('epic-form');
+
+function handleEpicSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    addEpic(formData.get('name'));
+
+    e.target.reset();
+    epicModal.close();
+    displayEpics(epicObjects);
+}
 
 
-const bug1 = new Ticket('Fix login crash', '06-08-2026', 'critical');
-const bug2 = new Ticket('Update border colors', '06-10-2026', 'low');
 
-authEpic.tickets.push(bug1);
-authEpic.tickets.push(bug2);
 
-displayEpics(epicObjects);
+createEpic.addEventListener('click', () => {
+    epicModal.showModal();
+});
+epicForm.addEventListener('submit', handleEpicSubmit);
 
 
 epicContainer.addEventListener('click', (e) => {
